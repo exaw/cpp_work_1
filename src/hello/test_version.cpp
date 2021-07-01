0ea0d6896f8aa2e3299037cd4d6d4b663fa9a90d
taynovai <dev.taynovai@gmail.com>
2021-07-01 23:03:43 +0500

    В код добавлен gtest

--- src/hello/test_version.cpp
+++ src/hello/test_version.cpp
@@ -1,7 +1,15 @@
 
+#include <gtest/gtest.h>
+
 #include "lib.h"
+
+TEST(test_version_1, test_version)
+{
+	ASSERT_GT(version(), 0);
+}
 
 int main()
 {
-    return version() > 0 ? 0 : 1;
+    //return version() > 0 ? 0 : 1;
+    return RUN_ALL_TESTS();
 }
